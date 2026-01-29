98bfadc0e4413707d3738bc70cd2f2e895173660
Dorkenoo Shadrack <shadrackdorkenoo@gmail.com>
2026-01-29 21:37:21 +0000

    Implemented Resources management

--- resources/__init_py.py
+++ resources/__init_py.py
@@ -1,3 +1,9 @@
 """
-Resource management package.
+Resources Package for Blockchain-Based Distributed Operating System
+
+This package provides resource management capabilities for CPU and storage allocation.
 """
+
+from .resources_manageer import ResourceManager
+
+__all__ = ['ResourceManager']
